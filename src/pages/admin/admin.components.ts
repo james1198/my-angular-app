a849e9f066a6ebc2e4b4ca4740da97afb9c3956a
james1198 <jblee@batestech.edu>
2026-07-02 10:56:03 -0700

    commit

---
+++ src/pages/admin/admin.components.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './admin.template.html',
+
+})
+export class AdminPage {
+
+}
---
+++ src/pages/home/home.components.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './home.template.html',
+    styleUrl: './home.styles.css'
+})
+export class HomePage {
+
+}
